6450124747ddae4ccbe0077a0b3ee52b5c3ceadc
JyotiMitkar55 <jyoti.mitkar55@gmail.com>
2022-01-16 19:29:16 +0530

    Removed print statement

--- TestApp/TestApp/AppDelegate.swift
+++ TestApp/TestApp/AppDelegate.swift
@@ -27,7 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
-        print("applicationDidEnterBackground called")
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
